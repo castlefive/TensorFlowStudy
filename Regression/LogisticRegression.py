3be27e4174216bc926b0a771f2257d328c441fa0
castlefive <castle_five@naver.com>
2019-05-23 17:34:25 +0900

    함수명 변경

--- Regression/LogisticRegression.py
+++ Regression/LogisticRegression.py
@@ -9,19 +9,19 @@ def logistic_regression(features):
     return hypothesis
 
 
-def loss_fn(hypothesis, features, labels):
+def get_loss(hypothesis, features, labels):
     cost = -tf.reduce_mean(labels * tf.log(logistic_regression(features))
                            + (1 - labels) * tf.log(1 - hypothesis))
     return cost
 
 
-def grad(features, labels):
+def calc_grad(features, labels):
     with tf.GradientTape() as tape:
-        loss_value = loss_fn(logistic_regression(features), features, labels)
+        loss_value = get_loss(logistic_regression(features), features, labels)
     return tape.gradient(loss_value, [W, b])
 
 
-def accuracy_fn(hypothesis, labels):
+def get_accuracy(hypothesis, labels):
     predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
     accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, labels), dtype=tf.int32))
     return accuracy
@@ -53,10 +53,10 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
 
 for step in range(1000):
     for score, result in tfe.Iterator(dataset):
-        grads = grad(score, result)
+        grads = calc_grad(score, result)
         optimizer.apply_gradients(grads_and_vars=zip(grads, [W, b]))
         if step % 100 == 0:
-            print(step, loss_fn(logistic_regression(score), score, result))
+            print(step, get_loss(logistic_regression(score), score, result))
 
-test_acc = accuracy_fn(logistic_regression(score_test), result_test)
+test_acc = get_accuracy(logistic_regression(score_test), result_test)
 print(test_acc)
